a3ebb1f5657881b574c0f6b9384daafdc7c5b4df
jeevashanmugam <jeevasj2000@gmail.com>
2023-06-27 21:45:33 +0530

    JS-for-sum-n-numbers

---
+++ main.js
@@ -0,0 +1,24 @@
+
+let a = 10;
+sum = 0;
+count = 0;
+for (i = 1; i <= a; i++) {
+    sum = sum + i
+    count++
+}
+
+document.write("Sum of the numbers is : " + sum + "<br>")
+document.write("Count of the numbers is : " + count + "<br>")
+document.write("Avgerage of the (sum) numbers is : " + sum / count + "<br>")
+
+
+
+
+
+
+
+
+
+
+
+
